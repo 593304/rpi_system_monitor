7644693aedad1720d505079b7ca6507be233d9f2
Adam <adam.593304@gmail.com>
2019-02-16 18:32:02 +0100

    Update 3 - Code cleanup

--- README.md
+++ README.md
@@ -3,108 +3,120 @@
 Simple python script for saving system values to database for future use
 
 Database schema for the script:
-
+```SQL
 CREATE TABLE monitoring.rpi_data (  
-	id BIGSERIAL PRIMARY KEY,  
-	cpu_temp_celsius NUMERIC(5, 2),  
-	cpu0_usage_percent NUMERIC(5, 2),  
-	cpu1_usage_percent NUMERIC(5, 2),  
-	cpu2_usage_percent NUMERIC(5, 2),  
-	cpu3_usage_percent NUMERIC(5, 2),  
-	mem_usage_mb NUMERIC(6, 2),  
-	mem_total_mb NUMERIC(6, 2),  
-	sd_card_usage_gb NUMERIC(6, 2),  
-	sd_card_total_gb NUMERIC(6, 2),  
-	nas_usage_gb NUMERIC(6, 2),  
-	nas_total_gb NUMERIC(6, 2),  
-	dev_usage_gb NUMERIC(6, 2),  
-	dev_total_gb NUMERIC(6, 2),  
-	cloud_usage_gb NUMERIC(6, 2),  
-	cloud_total_gb NUMERIC(6, 2),  
-	timestamp TIMESTAMP  
+    id BIGSERIAL PRIMARY KEY,  
+    cpu_temp_celsius NUMERIC(5, 2),  
+    cpu0_usage_percent NUMERIC(5, 2),  
+    cpu1_usage_percent NUMERIC(5, 2),  
+    cpu2_usage_percent NUMERIC(5, 2),  
+    cpu3_usage_percent NUMERIC(5, 2),  
+    mem_usage_mb NUMERIC(6, 2),  
+    mem_total_mb NUMERIC(6, 2),  
+    sd_card_usage_gb NUMERIC(6, 2),  
+    sd_card_total_gb NUMERIC(6, 2),  
+    nas_usage_gb NUMERIC(6, 2),  
+    nas_total_gb NUMERIC(6, 2),  
+    dev_usage_gb NUMERIC(6, 2),  
+    dev_total_gb NUMERIC(6, 2),  
+    cloud_usage_gb NUMERIC(6, 2),  
+    cloud_total_gb NUMERIC(6, 2),  
+    timestamp TIMESTAMP  
 )
+```
 
 ## Update 1
 
-Views for daily and hourly statistics for CPU and memory
-
+View for hourly average cpu and memory statistics
+```SQL
 CREATE VIEW monitoring.rpi_data_cpu_mem_hourly AS  
-	SELECT  
-		ROUND(AVG(cpu_temp_celsius), 2) AS cpu_temp_celsius,  
-		ROUND(AVG(cpu0_usage_percent), 2) AS cpu0_usage_percent,  
-		ROUND(AVG(cpu1_usage_percent), 2) AS cpu1_usage_percent,  
-		ROUND(AVG(cpu2_usage_percent), 2) AS cpu2_usage_percent,  
-		ROUND(AVG(cpu3_usage_percent), 2) AS cpu3_usage_percent,  
-		ROUND(AVG(mem_usage_mb), 2) AS mem_usage_mb,  
-		ROUND(AVG(mem_total_mb), 2) AS mem_total_mb,  
-		DATE_TRUNC('hour', timestamp) AS hour  
-	FROM  
-		monitoring.rpi_data  
-	GROUP BY  
-		hour  
-	ORDER BY  
-	hour ASC;
+    SELECT  
+        ROUND(AVG(cpu_temp_celsius), 2) AS cpu_temp_celsius,  
+        ROUND(AVG(cpu0_usage_percent), 2) AS cpu0_usage_percent,  
+        ROUND(AVG(cpu1_usage_percent), 2) AS cpu1_usage_percent,  
+        ROUND(AVG(cpu2_usage_percent), 2) AS cpu2_usage_percent,  
+        ROUND(AVG(cpu3_usage_percent), 2) AS cpu3_usage_percent,  
+        ROUND(AVG(mem_usage_mb), 2) AS mem_usage_mb,  
+        ROUND(AVG(mem_total_mb), 2) AS mem_total_mb,  
+        DATE_TRUNC('hour', timestamp) AS hour  
+    FROM  
+        monitoring.rpi_data  
+    GROUP BY  
+        hour  
+    ORDER BY  
+    hour ASC;
+```
 
+View for daily average cpu and memory statistics
+```SQL
 CREATE VIEW monitoring.rpi_data_cpu_mem_daily AS  
-	SELECT  
-		ROUND(AVG(cpu_temp_celsius), 2) AS cpu_temp_celsius,  
-		ROUND(AVG(cpu0_usage_percent), 2) AS cpu0_usage_percent,  
-		ROUND(AVG(cpu1_usage_percent), 2) AS cpu1_usage_percent,  
-		ROUND(AVG(cpu2_usage_percent), 2) AS cpu2_usage_percent,  
-		ROUND(AVG(cpu3_usage_percent), 2) AS cpu3_usage_percent,  
-		ROUND(AVG(mem_usage_mb), 2) AS mem_usage_mb,  
-		ROUND(AVG(mem_total_mb), 2) AS mem_total_mb,  
-		DATE_TRUNC('day', timestamp) AS day  
-	FROM  
-		monitoring.rpi_data  
-	GROUP BY  
-		day  
-	ORDER BY  
-	day ASC;
+    SELECT  
+        ROUND(AVG(cpu_temp_celsius), 2) AS cpu_temp_celsius,  
+        ROUND(AVG(cpu0_usage_percent), 2) AS cpu0_usage_percent,  
+        ROUND(AVG(cpu1_usage_percent), 2) AS cpu1_usage_percent,  
+        ROUND(AVG(cpu2_usage_percent), 2) AS cpu2_usage_percent,  
+        ROUND(AVG(cpu3_usage_percent), 2) AS cpu3_usage_percent,  
+        ROUND(AVG(mem_usage_mb), 2) AS mem_usage_mb,  
+        ROUND(AVG(mem_total_mb), 2) AS mem_total_mb,  
+        DATE_TRUNC('day', timestamp) AS day  
+    FROM  
+        monitoring.rpi_data  
+    GROUP BY  
+        day  
+    ORDER BY  
+    day ASC;
+```
 
 ## Update 2
 
-View for daily and hourly statistics for disk usage
-
+View for calculating the disk usage difference minute by minute
+```SQL
 CREATE VIEW monitoring.rpi_data_disk_diff_by_minutes AS  
-	SELECT  
-		(next.sd_card_usage_gb - base.sd_card_usage_gb) AS sd_card_usage_diff_gb,  
-		(next.nas_usage_gb - base.nas_usage_gb) AS nas_usage_diff_gb,  
-		(next.dev_usage_gb - base.dev_usage_gb) AS dev_usage_diff_gb,  
-		(next.cloud_usage_gb - base.cloud_usage_gb) AS cloud_usage_diff_gb,  
-		next.minute  
-	FROM  
-		(SELECT sd_card_usage_gb, nas_usage_gb, dev_usage_gb, cloud_usage_gb, DATE_TRUNC('minute', timestamp) AS minute FROM monitoring.rpi_data WHERE id < (SELECT MAX(id) FROM monitoring.rpi_data) ORDER BY minute) AS base,  
-		(SELECT sd_card_usage_gb, nas_usage_gb, dev_usage_gb, cloud_usage_gb, DATE_TRUNC('minute', timestamp) AS minute FROM monitoring.rpi_data WHERE id > (SELECT MIN(id) FROM monitoring.rpi_data) ORDER BY minute) AS next  
-	WHERE  
-		(base.minute + (interval '1 minute')) = next.minute  
-	ORDER BY  
-		next.minute ASC;
+    SELECT  
+        (next.sd_card_usage_gb - base.sd_card_usage_gb) AS sd_card_usage_diff_gb,  
+        (next.nas_usage_gb - base.nas_usage_gb) AS nas_usage_diff_gb,  
+        (next.dev_usage_gb - base.dev_usage_gb) AS dev_usage_diff_gb,  
+        (next.cloud_usage_gb - base.cloud_usage_gb) AS cloud_usage_diff_gb,  
+        next.minute  
+    FROM  
+        (SELECT sd_card_usage_gb, nas_usage_gb, dev_usage_gb, cloud_usage_gb, DATE_TRUNC('minute', timestamp) AS minute FROM monitoring.rpi_data WHERE id < (SELECT MAX(id) FROM monitoring.rpi_data) ORDER BY minute) AS base,  
+        (SELECT sd_card_usage_gb, nas_usage_gb, dev_usage_gb, cloud_usage_gb, DATE_TRUNC('minute', timestamp) AS minute FROM monitoring.rpi_data WHERE id > (SELECT MIN(id) FROM monitoring.rpi_data) ORDER BY minute) AS next  
+    WHERE  
+        (base.minute + (interval '1 minute')) = next.minute  
+    ORDER BY  
+        next.minute ASC;
+```
 
+View for disk usage differences by hours
+```SQL
 CREATE VIEW monitoring.rpi_data_disk_diff_by_hours AS  
-	SELECT  
-		SUM(sd_card_usage_diff_gb) AS sd_card_usage_diff_gb,  
-		SUM(nas_usage_diff_gb) AS nas_usage_diff_gb,  
-		SUM(dev_usage_diff_gb) AS dev_usage_diff_gb,  
-		SUM(cloud_usage_diff_gb) AS cloud_usage_diff_gb,   
-		DATE_TRUNC('hour', minute) AS hour  
-	FROM  
-		monitoring.rpi_data_disk_diff_by_minutes  
-	GROUP BY  
-		hour  
-	ORDER BY  
-		hour ASC;
+    SELECT  
+        SUM(sd_card_usage_diff_gb) AS sd_card_usage_diff_gb,  
+        SUM(nas_usage_diff_gb) AS nas_usage_diff_gb,  
+        SUM(dev_usage_diff_gb) AS dev_usage_diff_gb,  
+        SUM(cloud_usage_diff_gb) AS cloud_usage_diff_gb,   
+        DATE_TRUNC('hour', minute) AS hour  
+    FROM  
+        monitoring.rpi_data_disk_diff_by_minutes  
+    GROUP BY  
+        hour  
+    ORDER BY  
+        hour ASC;
+```
 
+View for disk usage differences by days
+```SQL
 CREATE VIEW monitoring.rpi_data_disk_diff_by_days AS  
-	SELECT  
-		SUM(sd_card_usage_diff_gb) AS sd_card_usage_diff_gb,  
-		SUM(nas_usage_diff_gb) AS nas_usage_diff_gb,  
-		SUM(dev_usage_diff_gb) AS dev_usage_diff_gb,  
-		SUM(cloud_usage_diff_gb) AS cloud_usage_diff_gb,   
-		DATE_TRUNC('day', minute) AS day  
-	FROM  
-		monitoring.rpi_data_disk_diff_by_minutes  
-	GROUP BY  
-		day  
-	ORDER BY  
-		day ASC;
+    SELECT  
+        SUM(sd_card_usage_diff_gb) AS sd_card_usage_diff_gb,  
+        SUM(nas_usage_diff_gb) AS nas_usage_diff_gb,  
+        SUM(dev_usage_diff_gb) AS dev_usage_diff_gb,  
+        SUM(cloud_usage_diff_gb) AS cloud_usage_diff_gb,   
+        DATE_TRUNC('day', minute) AS day  
+    FROM  
+        monitoring.rpi_data_disk_diff_by_minutes  
+    GROUP BY  
+        day  
+    ORDER BY  
+        day ASC;
+```
--- get_rpi_data.py
+++ get_rpi_data.py
@@ -1,58 +1,150 @@
 #!/usr/bin/env python3
 
+import logging
 import psutil
 import psycopg2
+import json
+import os
 
-conn = psycopg2.connect("dbname=adam user=adam")
-cur = conn.cursor()
+DRIVES = {
+    'sd_card': '/',
+    'nas': '/mnt/nas',
+    'dev': '/mnt/dev',
+    'cloud': '/mnt/cloud'
+}
 
-# Getting the values in the beginning and in the end to get the average of them
-cpuTemp = psutil.sensors_temperatures()['cpu-thermal'][0].current
-memUsage = psutil.virtual_memory().used/1024/1024
-diskUsage = psutil.disk_usage('/').used/1024/1024/1024
-nasUsage = psutil.disk_usage('/mnt/nas').used/1024/1024/1024
-devUsage = psutil.disk_usage('/mnt/dev').used/1024/1024/1024
-cloudUsage = psutil.disk_usage('/mnt/cloud').used/1024/1024/1024
+DB_CONNECTION = None
+DB_CURSOR = None
+TEMP_DB_FILE = '/mnt/dev/monitoring/RPI_data/temp_db'
+FILE = None
 
-# Getting the percents first, because it takes multiple seconds to gather the information
-cpuPercents = psutil.cpu_percent(interval=5, percpu=True)
-cpu0Usage = cpuPercents[0]
-cpu1Usage = cpuPercents[1]
-cpu2Usage = cpuPercents[2]
-cpu3Usage = cpuPercents[3]
+LOGGER = None
+LOG_FILE = '/mnt/dev/log/python/rpi_data_polling.log'
+LOGGER_FORMAT = '%(asctime)15s | %(levelname)8s | %(name)s - %(funcName)12s - %(message)s'
 
-# Calculating the values
-cpuTemp = (psutil.sensors_temperatures()['cpu-thermal'][0].current + cpuTemp) / 2
-memUsage = (psutil.virtual_memory().used/1024/1024 + memUsage) / 2
-memTotal = psutil.virtual_memory().total/1024/1024
-diskUsage = (psutil.disk_usage('/').used/1024/1024/1024 + diskUsage) / 2
-diskTotal = psutil.disk_usage('/').total/1024/1024/1024
-nasUsage = (psutil.disk_usage('/mnt/nas').used/1024/1024/1024 + nasUsage) / 2
-nasTotal = psutil.disk_usage('/mnt/nas').total/1024/1024/1024
-devUsage = (psutil.disk_usage('/mnt/dev').used/1024/1024/1024 + devUsage) / 2
-devTotal = psutil.disk_usage('/mnt/dev').total/1024/1024/1024
-cloudUsage = (psutil.disk_usage('/mnt/cloud').used/1024/1024/1024 + cloudUsage) / 2
-cloudTotal = psutil.disk_usage('/mnt/cloud').total/1024/1024/1024
 
-# For debugging
-#print ("cpu temp   : {0: >8.2f}°C".format(cpuTemp))
-#print ("cpu0 usage : {0: >8.2f}%".format(cpu0Usage))
-#print ("cpu1 usage : {0: >8.2f}%".format(cpu1Usage))
-#print ("cpu2 usage : {0: >8.2f}%".format(cpu2Usage))
-#print ("cpu3 usage : {0: >8.2f}%".format(cpu3Usage))
-#print ("mem usage  : {0: >8.2f}MB".format(memUsage))
-#print ("mem total  : {0: >8.2f}MB".format(memTotal))
-#print ("disk usage : {0: >8.2f}GB".format(diskUsage))
-#print ("disk total : {0: >8.2f}GB".format(diskTotal))
-#print ("nas usage  : {0: >8.2f}GB".format(nasUsage))
-#print ("nas total  : {0: >8.2f}GB".format(nasTotal))
-#print ("dev usage  : {0: >8.2f}GB".format(devUsage))
-#print ("dev total  : {0: >8.2f}GB".format(devTotal))
-#print ("cloud usage: {0: >8.2f}GB".format(cloudUsage))
-#print ("cloud total: {0: >8.2f}GB".format(cloudTotal))
+def init():
+    logging.basicConfig(filename=LOG_FILE, format=LOGGER_FORMAT, level=logging.INFO)
+    global LOGGER
+    LOGGER = logging.getLogger('polling_rpi_system')
+    try:
+        global DB_CONNECTION
+        DB_CONNECTION = psycopg2.connect('dbname=adam user=adam')
+        global DB_CURSOR
+        DB_CURSOR = DB_CONNECTION.cursor()
+        LOGGER.debug('Connected to the database')
+        LOGGER.debug('Checking temp file')
+        check_temp_file()
+    except Exception:
+        LOGGER.error('Cannot connect to the database, using the temporary file')
+        global FILE
+        FILE = open(TEMP_DB_FILE, 'a+')
 
-# Saving values to DB
-cur.execute('INSERT INTO monitoring.rpi_data(cpu_temp_celsius,cpu0_usage_percent,cpu1_usage_percent,cpu2_usage_percent,cpu3_usage_percent,mem_usage_mb,mem_total_mb,sd_card_usage_gb,sd_card_total_gb,nas_usage_gb,nas_total_gb,dev_usage_gb,dev_total_gb,cloud_usage_gb,cloud_total_gb,timestamp) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,now());', (cpuTemp, cpu0Usage, cpu1Usage, cpu2Usage, cpu3Usage, memUsage, memTotal, diskUsage, diskTotal, nasUsage, nasTotal, devUsage, devTotal, cloudUsage, cloudTotal))
-conn.commit()
-cur.close()
-conn.close()
+
+def check_temp_file():
+    try:
+        file = open(TEMP_DB_FILE, 'r')
+        lines = file.readlines()
+        LOGGER.info('Found temporary file with {} records'.format(len(lines)))
+        file.close()
+        for line in lines:
+            save_to_db(json.loads(line))
+        os.remove(TEMP_DB_FILE)
+    except Exception:
+        pass
+
+
+def save_to_file(data):
+    LOGGER.debug('Saving to file')
+    FILE.write(json.dumps(data) + '\n')
+
+
+def save_to_db(data):
+    LOGGER.debug('Saving to DB ')
+    DB_CURSOR.execute(
+        'INSERT INTO '
+        '  monitoring.rpi_data('
+        '    cpu_temp_celsius, cpu0_usage_percent, cpu1_usage_percent, cpu2_usage_percent, cpu3_usage_percent, '
+        '    mem_usage_mb, mem_total_mb, '
+        '    sd_card_usage_gb, sd_card_total_gb, '
+        '    nas_usage_gb, nas_total_gb, '
+        '    dev_usage_gb, dev_total_gb, '
+        '    cloud_usage_gb, cloud_total_gb, '
+        '    timestamp) '
+        'VALUES('
+        '  %s, %s, %s, %s, %s, '
+        '  %s, %s, '
+        '  %s, %s, '
+        '  %s, %s, '
+        '  %s, %s, '
+        '  %s, %s, '
+        '  now());',
+        (data['cpu_temp'], data['cpu_0_usage'], data['cpu_1_usage'], data['cpu_2_usage'], data['cpu_3_usage'],
+         data['mem_usage'], data['mem_total'],
+         data['disk_usage'], data['disk_total'],
+         data['nas_usage'], data['nas_total'],
+         data['dev_usage'], data['dev_total'],
+         data['cloud_usage'], data['cloud_total']))
+    DB_CONNECTION.commit()
+
+
+def to_mb(in_bytes):
+    return in_bytes / 1024 / 1024
+
+
+def to_gb(in_bytes):
+    return in_bytes / 1024 / 1024 / 1024
+
+
+def main():
+    # Getting the values in the beginning and in the end to get the average of them
+    cpu_temp = psutil.sensors_temperatures()['cpu-thermal'][0].current
+    mem_usage = to_mb(psutil.virtual_memory().used)
+    disk_usage = to_gb(psutil.disk_usage(DRIVES['sd_card']).used)
+    nas_usage = to_gb(psutil.disk_usage(DRIVES['nas']).used)
+    dev_usage = to_gb(psutil.disk_usage(DRIVES['dev']).used)
+    cloud_usage = to_gb(psutil.disk_usage(DRIVES['cloud']).used)
+
+    # Getting the percents first, because it takes multiple seconds to gather the information
+    cpu_percents = psutil.cpu_percent(interval=15, percpu=True)
+    cpu_0_usage = cpu_percents[0]
+    cpu_1_usage = cpu_percents[1]
+    cpu_2_usage = cpu_percents[2]
+    cpu_3_usage = cpu_percents[3]
+
+    # Calculating the values
+    cpu_temp = (psutil.sensors_temperatures()['cpu-thermal'][0].current + cpu_temp) / 2
+    mem_usage = (to_mb(psutil.virtual_memory().used) + mem_usage) / 2
+    mem_total = to_mb(psutil.virtual_memory().total)
+    disk_usage = (to_gb(psutil.disk_usage(DRIVES['sd_card']).used) + disk_usage) / 2
+    disk_total = to_gb(psutil.disk_usage(DRIVES['sd_card']).total)
+    nas_usage = (to_gb(psutil.disk_usage(DRIVES['nas']).used) + nas_usage) / 2
+    nas_total = to_gb(psutil.disk_usage(DRIVES['nas']).total)
+    dev_usage = (to_gb(psutil.disk_usage(DRIVES['dev']).used) + dev_usage) / 2
+    dev_total = to_gb(psutil.disk_usage(DRIVES['dev']).total)
+    cloud_usage = (to_gb(psutil.disk_usage(DRIVES['cloud']).used) + cloud_usage) / 2
+    cloud_total = to_gb(psutil.disk_usage(DRIVES['cloud']).total)
+
+    data = {
+        'cpu_temp': cpu_temp,
+        'cpu_0_usage': cpu_0_usage, 'cpu_1_usage': cpu_1_usage, 'cpu_2_usage': cpu_2_usage, 'cpu_3_usage': cpu_3_usage,
+        'mem_usage': mem_usage, 'mem_total': mem_total,
+        'disk_usage': disk_usage, 'disk_total': disk_total,
+        'nas_usage': nas_usage, 'nas_total': nas_total,
+        'dev_usage': dev_usage, 'dev_total': dev_total,
+        'cloud_usage': cloud_usage, 'cloud_total': cloud_total}
+
+    LOGGER.debug('RPI data: {}'.format(data))
+
+    if DB_CONNECTION is None:
+        save_to_file(data)
+        FILE.close()
+    else:
+        save_to_db(data)
+        DB_CURSOR.close()
+        DB_CONNECTION.close()
+
+
+if __name__ == '__main__':
+    init()
+    main()
